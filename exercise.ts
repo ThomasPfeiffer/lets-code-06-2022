b1a6026521b9a3f2760e7f5fda4b56495caa55e3
Thomas Pfeiffer <thomas.pfeiffer@lise.de>
2022-06-23 15:52:31 +0200

    Initialized ReadMe

---
+++ exercise.ts
@@ -0,0 +1,18 @@
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  size: {
+    height: number;
+    weight: number;
+  };
+}
+
+interface Animal {
+  name: string;
+  age: number;
+  size: {
+    height: number;
+    weight: number;
+  };
+}
