1f4fadb5d86b96e27990897dbcd7be5478dad2b8
Thomas Pfeiffer <thomas.pfeiffer@lise.de>
2022-06-23 16:01:34 +0200

    Implemented solution

--- exercise.ts
+++ exercise.ts
@@ -16,3 +16,33 @@ interface Animal {
     weight: number;
   };
 }
+
+// Union Types
+
+type Creature = Person | Animal;
+
+function greet(creature: Creature) {
+  if (isPerson(creature)) {
+    return `Hello, ${creature.firstName} ${creature.lastName}!`;
+  }
+  return `Hello, ${creature.name}!`;
+}
+
+function isPerson(creature: Creature): creature is Person {
+  return "firstName" in creature && "lastName" in creature;
+}
+
+// Intersection Types
+type Alien = Person & Animal;
+
+// Pick
+type Appearance = Pick<Person, "size" | "age">;
+
+// Omit
+type Fly = Omit<Animal, "size">;
+
+// Partial
+type NewPersion = Partial<Person>;
+
+// Record
+type Enclosures = Record<number, Animal["name"]>;
